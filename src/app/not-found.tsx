ef4693cbba20f036dd5a4fc15986f688b92a477f
tami-cp0 <findtamilore>
2025-09-06 10:34:53 +0100

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <main className='min-w-screen min-h-screen flex flex-col justify-center items-center gap-4 font-mono'>
+        <h1 className='text-7xl'>404</h1>
+        <p>Page Not Found</p>
+    </main>
+  )
+}
+
+export default NotFound;
