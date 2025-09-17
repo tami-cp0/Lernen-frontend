4ba181e597686c69a040bc214f2d2b05c1857ef5
tami-cp0 <findtamilore@gmail.com>
2025-09-17 21:05:44 +0100

    initialize google env

--- env.client.ts
+++ env.client.ts
@@ -1,4 +1,5 @@
 // This file is used to define environment variables that are safe to expose to the client-side code.
 export const clientEnv = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
 };
