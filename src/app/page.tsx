c8effbb7f52d8a1cc370beb013c69a35b50fe902
tami-cp0 <findtamilore@gmail.com>
2025-09-17 21:06:02 +0100

    remove unused metadate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "  Lernen",
-  description: "<later>",
-  icons: {
-    icon: "/favicon.svg",
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Lernen",
+//   description: "<later>",
+//   icons: {
+//     icon: "/favicon.svg",
+//   },
+// };
 
 // design the card for twitter and linkedin
 
